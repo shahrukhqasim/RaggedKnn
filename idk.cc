9650d1f5866c1d79ce287b017f7e58fb7ecf7cd6
Shah Rukh Qasim <14beesqasim@seecs.edu.pk>
2019-11-20 21:37:39 +0100

    feat: Add the CPU implementation

--- idk.cc
+++ idk.cc
@@ -4,11 +4,52 @@
 
 #include <new>
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
+// Redefinition
+struct combined {
+    int index;
+    float distance;
+};
+class combinedcomparator {
+public:
+    int operator() (const combined& p1, const combined& p2)
+    {
+        return p1.distance < p2.distance;
+    }
+};
+
+
 
 int main() {
-    cout<<"Hello, world!"<<endl;
+    std::vector<float> data = {2, 8, 7, 5, 9, 3, 6, 1, 10, 4};
+    int k=4;
+
+    std::priority_queue <combined, std::vector<combined>, combinedcomparator> topneighbors;
+    for (int i = 0; i < data.size(); i++) {
+        if (topneighbors.size()<k) {
+            topneighbors.push({i, data[i]});
+        }
+        else if (topneighbors.top().distance > data[i]) {
+            topneighbors.pop();
+            topneighbors.push({i, data[i]});
+        }
+    }
+
+    std::vector<float> res(k);
+    for (int i = 0; i < k; ++i) {
+        res[k - i - 1] = topneighbors.top().distance;
+        topneighbors.pop();
+    }
+    for (int i = 0; i < k; ++i) {
+        std::cout<< res[i] <<std::endl;
+    }
+
+
+
+
     return 0;
 }
--- ragged_knn_kernel.cc
+++ ragged_knn_kernel.cc
@@ -5,6 +5,8 @@
 
 #include "tensorflow/core/framework/op_kernel.h"
 #include "ragged_knn_kernel.h"
+#include <queue>
+
 
 
 namespace tensorflow {
@@ -13,18 +15,55 @@ namespace tensorflow {
 
 
     namespace functor {
+
+        // Redefinition
+        struct combined {
+            int index;
+            float distance;
+        };
+        class combinedcomparator {
+        public:
+            int operator() (const combined& p1, const combined& p2)
+            {
+                return p1.distance < p2.distance;
+            }
+        };
+
         // CPU specialization null
         template<typename dummy>
         struct RaggedKnnOpFunctor<CPUDevice, dummy> {
             void operator()(const CPUDevice &d, const float *d_data, const int *d_row_splits, int *d_output_indices,
                     float *d_output_distance, int num_neighbors, int num_features, int num_batch, int num_total_vertices, bool add_splits) {
 //                assert(false); // We don't have a CPU implementation sorry :(
-                printf("Running CPU implementation but its nothing here sorry!\n");
+                printf("Running CPU implementation (in testing)!\n");
 
                 for(int batch = 0; batch < num_batch; batch++) {
                     int num_vertices_in_batch = d_row_splits[batch-1] - d_row_splits[batch];
                     for(int i_vertex_in_batch = 0; i_vertex_in_batch < num_vertices_in_batch; i_vertex_in_batch++) {
+                        std::priority_queue <combined, std::vector<combined>, combinedcomparator> topneighbors;
 
+                        for(int j_vertex_in_batch = 0; j_vertex_in_batch < num_vertices_in_batch; j_vertex_in_batch++) {
+                            int index = j_vertex_in_batch;
+                            float distance = 0;
+                            for(int i_feature = 0; i_feature < num_vertices_in_batch; i_feature++) {
+                                float a = d_data[i_vertex_in_batch*num_features + i_feature];
+                                float b = d_data[j_vertex_in_batch*num_features + i_feature];
+                                distance += (a-b)*(a+b);
+                            }
+
+                            if (topneighbors.size() < num_neighbors) {
+                                topneighbors.push({index, distance});
+                            }
+                            else if(topneighbors.top().distance > distance) {
+                                topneighbors.pop();
+                                topneighbors.push({index, distance});
+                            }
+                        }
+
+                        for(int j_top_neighbor = 0; j_top_neighbor< num_neighbors; j_top_neighbor++) {
+                            d_output_distance[i_vertex_in_batch*(num_neighbors+1)-j_top_neighbor-1] = topneighbors.top().distance;
+                            d_output_indices[i_vertex_in_batch*(num_neighbors+1)-j_top_neighbor-1] = topneighbors.top().index;
+                        }
                     }
                 }
             }
